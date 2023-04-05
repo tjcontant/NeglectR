1c67e34cc1c17a23b02a39ee174c094b4dadb2e9
Tristan J. Contant <tjcontant@icloud.com>
2023-04-05 15:30:00 -0400

    added new tests for df_apply and updated

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -5,3 +5,16 @@ test_that("numeric apply works", {
   expect_equal(df_apply(data, func, filt), 
                data.frame(nums = c(2, 4, 6, 8, 10, 12, 14)))
 })
+
+test_that("trying alternative function parameter", {
+  data <- data.frame(nums = c("a", "abc", "cat"))
+  func <- function(.int) {return("123")}
+  expect_equal(df_apply(data, func, is.character), 
+               data.frame(nums = c("123", "123", "123")))
+})
+
+test_that("passed non-dataframe correctly", {
+  data <- 2
+  func <- function(.int) {return(.int * 2)}
+  expect_error(df_apply(data = 2, func, is.numeric))
+})
